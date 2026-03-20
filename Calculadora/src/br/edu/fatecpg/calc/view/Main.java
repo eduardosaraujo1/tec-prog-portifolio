6fdc0dffc12183a1036b579a08216bfa6d489bf4
eduardosaraujo1 <eduardosaraujo1@proton.me>
2026-03-20 14:29:12 -0300

    atividade calculadora

---
+++ Calculadora/src/br/edu/fatecpg/calc/view/Main.java
@@ -0,0 +1,15 @@
+package br.edu.fatecpg.calc.view;
+
+import br.edu.fatecpg.calc.model.Calculadora;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Calculadora calc = new Calculadora();
+		
+		System.out.println("A soma é: " + calc.somar(5, 2));
+		System.out.println("A subtração é: " + calc.subtrair(5, 2));
+		System.out.println("A multiplicação é: " + calc.multiplicar(5, 2));
+		System.out.println("A divisão é: " + calc.dividir(5, 2));
+	}
+}
