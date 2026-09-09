abc5bae75d9135b1beb4c4d65b0d9c09254b1101
eduardosaraujo1 <eduardosaraujo1@proton.me>
2026-09-08 21:46:43 -0300

    task: modelagem produto

--- Tarefa1/app/src/main/java/br/edu/fatecpg/streamlambda/App.java
+++ Tarefa1/app/src/main/java/br/edu/fatecpg/streamlambda/App.java
@@ -1,8 +1,10 @@
 package br.edu.fatecpg.streamlambda;
 
+import br.edu.fatecpg.streamlambda.model.Produto;
+
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        System.out.println(new Produto("Teste", "Teste", 9));
     }
 }
